1e981a5abea44c3d7b181d83ce06a07b1160b181
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-19 23:18:53 +0000

    get traveller location

--- Tourism Recommendation Engine.py
+++ Tourism Recommendation Engine.py
@@ -1,2 +1,23 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 test_traveller = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
+attractions = []
+
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+
+def get_traveller_location(traveller):
+    traveller_destination = traveller[1]
+    traveller_destination_index = get_destination_index(traveller_destination)
+    return traveller_destination_index
+
+test_destination_index = get_traveller_location(test_traveller)
+
+print(test_destination_index)
+
+for destination in destinations:
+    attractions.append([])
+    print(attractions)
+
+def add_attraction(destination, attraction):
