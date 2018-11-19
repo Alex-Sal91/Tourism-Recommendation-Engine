0d88ea4a6edd1bf62fe75d5f31990246946b9be4
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-19 20:35:00 +0000

    Added test objects

---
+++ Tourism Recommendation Engine.py
@@ -0,0 +1,2 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveller = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
